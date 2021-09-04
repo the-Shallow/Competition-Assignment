bdaa9d0bcd378f7a14ffc539e4891b8bcb0542a0
the-Shallow <theshallow27@gmail.com>
2021-09-05 00:39:41 +0530

    Made few Changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV == 'development') {
     app.use(morgan("dev"));
 }
 
-app.use(helemt());
+app.use(helmet());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
