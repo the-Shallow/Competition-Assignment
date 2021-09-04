9e2668e91d5091e96d871f599acd8c438dda2be8
the-Shallow <theshallow27@gmail.com>
2021-09-05 00:37:28 +0530

    Made few Changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const competitionRouter = require('./routes/competitionRoutes');
 const userRoutes = require('./routes/userRoutes');
 const submissionRouter = require('./routes/submissionRoutes');
@@ -11,8 +12,10 @@ if (process.env.NODE_ENV == 'development') {
     app.use(morgan("dev"));
 }
 
+app.use(helemt());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
 
 app.use('/like', submissionLikeRouter);
 app.use('/submission', submissionRouter);
